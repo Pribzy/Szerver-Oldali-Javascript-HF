40b417ed7f38918d3db094ca5995935e5560d575
Leviathan <pribelszkilevi@gmail.com>
2018-10-10 13:48:56 +0200

    make route files

---
+++ myShop-Web/routes/outsideRoutes.js
@@ -0,0 +1,10 @@
+//Middlewares
+const authMW = require("../middlewares/auth");
+
+module.exports = function(app) {
+  const objectRepository = {};
+
+  //Route-ok
+  //GET: app.get
+  //POST:app.post
+};
