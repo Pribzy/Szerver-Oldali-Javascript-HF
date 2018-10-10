664b6c777e789596fff8ccf94c68e496451db4aa
Leviathan <pribelszkilevi@gmail.com>
2018-10-10 13:15:47 +0200

    delete index.js

--- myShop-Web/app.js
+++ myShop-Web/app.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var app = express();
 
+
+
 app.use(express.static('static'));
 
+
+//Middlewares
+const authMW = require('../middlewares/auth')
+
+module.exports = function(app){
+
+    const objRepo  = {}
+
+    //Route-ok
+    //GET: app.get
+    //POST:app.post
+}
 
 var server = app.listen(3000,function(){
     console.log("Server On: 3000");
