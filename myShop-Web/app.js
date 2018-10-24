790bd84fe8096a12e8d992a405696d39e8ad2a7a
Leviathan <pribelszkilevi@gmail.com>
2018-10-24 13:03:05 +0200

    make all .ejs

--- myShop-Web/app.js
+++ myShop-Web/app.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static("static"));
+//TODO: Átirányítani a 'views'-ra
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
 
 require("../routes/productRoutes.js")(app);
 require("../routes/storeRoutes.js")(app);
