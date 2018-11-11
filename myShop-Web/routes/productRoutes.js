9283ad0d9ec8972dfc27def055554bad1e53aafd
Leviathan <pribelszkilevi@gmail.com>
2018-11-11 12:38:40 +0100

    Bugfixes

--- myShop-Web/routes/productRoutes.js
+++ myShop-Web/routes/productRoutes.js
@@ -9,7 +9,7 @@ const getSearchedProductsMW = require("../middlewares/products/getSearchedProduc
 const saveProductMW = require("../middlewares/products/saveProduct");
 
 //Modellek
-var storeModel = require("../model/product");
+var productModel = require("../model/product");
 
 module.exports = function(app) {
   const objectRepository = {
