08ffcb5d019b5c433ab858a740122aaa1daaf993
Leviathan <pribelszkilevi@gmail.com>
2018-11-11 12:21:17 +0100

    Set header names

--- myShop-Web/middlewares/stores/getOneStore.js
+++ myShop-Web/middlewares/stores/getOneStore.js
@@ -1,10 +1,8 @@
 /**
-* Visszaadja az URL paraméterben megadott egy bolt adatait
-*/
-module.exports = function (objectrepository) {
-
-    return function (req, res, next) {
-        return next();
-    };
-
+ * Visszaadja az URL paraméterben megadott egy bolt adatait
+ */
+module.exports = function(objectrepository) {
+  return function(req, res, next) {
+    return next();
+  };
 };
